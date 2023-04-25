5dd49c51022d12b17622b3270cf6827734690df8
Mirza Ghulam Rasyid <mirza.rasyid@kobkiat-it.com>
2023-04-26 01:11:07 +0700

    Add project files.

---
+++ Obo.Web.App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Obo.Web.App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
